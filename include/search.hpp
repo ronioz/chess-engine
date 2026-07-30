0afe31b0d026faa322c83925845399fb730ff094
ronioz <roniosipov7@gmail.com>
2026-07-30 13:51:38 +0400

    updated MVV-LVA optimization

--- include/search.hpp
+++ include/search.hpp
@@ -6,6 +6,15 @@
 #include <limits.h>
 #include <algorithm>
 
+/*
+Reordering using MVV-LVA (Most valuable victim - Less valuable attacker pruning)
+
+The values are: 
+{PAWN = 100, KNIGHT = 290, BISHOP = 310, ROOK = 500, QUEEN = 900, KING = 0}
+
+If no capture occurs, then the score is set to -1
+The formula is: 10 * values[captured] - values[moved]
+*/
 void reorderLegalMoves(Board& board, std::vector<Move>& legalMoves);
 int negamax(Board& board, int alpha, int beta, int depth);
 Move findBestMove(Board& board, int depth);
